4974078e2d9543d65d5b5b2bfcf29abfc0a3943a
Timm Bäder <tbaeder@redhat.com>
2026-03-12 06:05:56 +0100

    2

--- clang/lib/AST/ByteCode/Compiler.cpp
+++ clang/lib/AST/ByteCode/Compiler.cpp
@@ -3517,6 +3517,10 @@ bool Compiler<Emitter>::VisitMaterializeTemporaryExpr(
   bool IsStatic = E->getStorageDuration() == SD_Static;
   if (IsStatic ||
       (ExtendingDecl && Context::shouldBeGloballyIndexed(ExtendingDecl))) {
+
+    if (this->constantFolding())
+      return false;
+
     UnsignedOrNone GlobalIndex = P.createGlobal(E, Inner->getType());
     if (!GlobalIndex)
       return false;
@@ -5426,6 +5430,8 @@ const Function *Compiler<Emitter>::getFunction(const FunctionDecl *FD) {
 
 template <class Emitter>
 bool Compiler<Emitter>::visitExpr(const Expr *E, bool DestroyToplevelScope) {
+  assert(E);
+  assert(!E->getType().isNull());
   LocalScope<Emitter> RootScope(this, ScopeKind::FullExpression);
 
   auto maybeDestroyLocals = [&]() -> bool {
